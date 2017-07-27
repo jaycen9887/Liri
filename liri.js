e9196324f04e8a5f44d610ac3f83e92b1886c213
Jaycen <jaycen9887@gmail.com>
2017-07-27 08:56:04 -0400

    finished

--- liri.js
+++ liri.js
@@ -12,38 +12,42 @@ var imdbKey = apiKeys.imdb_key.api_key;
 var twitterKey = apiKeys.twitter_key;
 var spotifyKey = apiKeys.spotify_key;
 
-if (process.argv[2] === "my-tweets"){
-    //shows last 20 tweets
-    twit();
-} else if(process.argv[2] === "spotify-this-song"){
-    // takes a song and returns the artist, song name, preview link from spotify, and the album the song is from. 
-    if(process.argv[3]){
-        song(process.argv[3]);
-    }else {
-        song("The Sign");
+var title = process.argv[3]; 
+
+function check(command){
+    switch(command){
+        case "my-tweets":
+            twit();
+            break;
+        case "spotify-this-song":
+            if(title){
+                song(title);
+            }else {
+                song("The Sign");
+            }
+            break;
+        case "movie-this":
+            if(title){
+                movie(title);
+            }else {
+                movie("Mr. Nobody");
+            }
+            break;
+        case "do-what-it-says":
+            fs.readFile("random.txt", "utf8", function(error, data){
+                if(!error){
+                    var n = data.split(",");
+                    title = n[1];
+                    check(n[0]);
+                    
+                    
+                }
+            });
+            break;            
     }
-    
-    //if no song is provided default is "The Sign" by Ace of Base
-} else if(process.argv[2] === "movie-this"){
-    //takes a movie title
-    //returns the title of the movie, year the movie came out, IMDB rating, rotten tomatoes rating, country movie was produced, language of the movie, plot of the movie, actors in the movie. 
-    //if no movie is provided default is "Mr. Nobody
-    if(process.argv[3]){
-       movie(process.argv[3]);
-    }else {
-        movie("Mr. Nobody");
-    }
-    
-} else if(process.argv[2] === "do-what-it-says"){
-    // reads the text inside random.txt and uses that to call a command
-    fs.readFile("random.txt", "utf8", function(error, data){
-        if(!error){
-            
-        }
-    })
-    
 }
 
+check(process.argv[2]);
 
 function movie(title) {
   imdb.get(title, {apiKey: imdbKey }, results);
